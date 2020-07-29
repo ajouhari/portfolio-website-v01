537672a8f5fa355bbbaa99f7014f48e07dee24e7
Abdulrahman Jouhari <official@ajouhari.com>
2020-07-29 19:58:48 +0300

    compressed-js-and-css

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const { src, series, parallel, dest, watch } = require('gulp');
+const jsPath = 'assets/js/*.js';
+const cssPath = 'assets/css/*.css';
+function htmlTask() {
+    return src('pages/*.html').pipe(gulp.dest('dist'));
+}
+function imgTask() {
+    return src('resources/images/*/*').pipe(gulp.dest('dist/images'));
+}
+function jsTask() {
+    return src(jsPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/assets/js'));
+}
+function cssTask() {
+    return src(cssPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/assets/css'));
+}
+function watchTask() {
+    watch([cssPath, jsPath], { interval: 1000 }, parallel(cssTask, jsTask));
+}
+exports.htmlTask = htmlTask;
+exports.imgTask = imgTask;
+exports.jsTask = jsTask;
+exports.cssTask = cssTask;
+exports.default = series(parallel(jsTask, cssTask), watchTask);
